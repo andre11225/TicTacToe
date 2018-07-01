ac383f7a28264fe71970bb213a3e6aa4f707690a
andre11225 <andre11225@yahoo.com>
2018-07-01 18:51:58 -0400

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -13,7 +13,7 @@ line = "--------------------"
 sec = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 
 
-
+# Function that will create the game board. . .
 def Board():
         print (space, sec[0], space, wall, space, sec[1], space, wall, space, sec[2], space)
         print (line)
